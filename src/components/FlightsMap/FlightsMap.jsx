884d10f64bea2dc81c7381657ab2dfe1c6d0a0d7
fediaz3 <fediaz3@uc.cl>
2021-05-22 10:51:43 -0400

    detail

--- src/components/FlightsMap/FlightsMap.jsx
+++ src/components/FlightsMap/FlightsMap.jsx
@@ -2,12 +2,12 @@ import React, {useState, useEffect} from 'react'
 import { MapContainer, TileLayer, Marker, Popup, CircleMarker, Polyline, Tooltip} from 'react-leaflet'
 import L from 'leaflet'
 import marker from "./plane.png";
+
 const io = require("socket.io-client");
 const newicon = new L.icon({
   iconUrl: marker,
-  iconSize: [15, 15]
+  iconSize: [22, 22]
 });
-
 
 const FlightsMap = (props) => {   
     const [positions, setPositions] = useState({})
@@ -36,26 +36,21 @@ const FlightsMap = (props) => {
 
     }, []);
 
+    // useEffect(() => {
+    //   // console.log("positions modificado:", positions)
+    //   // Object.entries(positions).map( x => {
+    //   //   // console.log(x[0], x[1][0], x[1][1])}
+    //   // )
+    //   // console.log('--------------------------------------')
+    // }, [positions])
 
-    useEffect(() => {
-      // console.log("positions modificado:", positions)
-      // Object.entries(positions).map( x => {
-      //   // console.log(x[0], x[1][0], x[1][1])}
-      // )
-      // console.log('--------------------------------------')
-    }, [positions])
-
-
-
-  
-    const limeOptions1 = { color: 'red', weight: 5 }
-    // const limeOptions2 = {color: "blue", weight: 3}
+    const limeOptions1 = { color: 'red', weight: 8 }
 
     const positionsView = Object.entries(positions).map( x => {
       // console.log("posiciones a mostrar: ", x[1])
-      if (x[1][0] == undefined || x[1][1] == undefined){
-        return false
-      }
+      // if (x[1][0] == undefined || x[1][1] == undefined){
+      //   return false
+      // }
       let positionsToShow = x[1].map(x => x)
       let lastPosition = positionsToShow[positionsToShow.length - 1]
       // console.log("positions to show:", positionsToShow)
@@ -74,24 +69,17 @@ const FlightsMap = (props) => {
     
     
     return (
-      <>  
-      
       <div id="FlightMap" >
         {/*<p>{position.code} {position.position}</p> */}
-        
         <MapContainer center={[51.505, -0.09]} zoom={1} scrollWheelZoom={true}>
             <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-            { positions != {} && positionsView }
+            { positionsView }
 
           </MapContainer>
       </div>
-      
-      </>
     );
-
-
 }
 
 
