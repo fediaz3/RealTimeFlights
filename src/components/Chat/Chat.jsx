d30121214c2556968790cb1fcb687ac068308d22
fediaz3 <fediaz3@uc.cl>
2021-05-22 14:00:33 -0400

    chatFirstAdvance

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,16 +1,86 @@
 import React, {useState, useEffect} from 'react'
 
+import { MessageList } from 'react-chat-elements'
+import { SystemMessage } from 'react-chat-elements'
+
+import { MessageBox } from 'react-chat-elements'
+
+
+
+const messages1 = [
+  {
+    position: 'left',
+    type: 'text',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    date: new Date(),
+  },
+  {
+    position: 'left',
+    type: 'text',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    date: new Date(),
+  },
+  {
+    position: 'left',
+    type: 'text',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    date: new Date(),
+  },
+  
+  {
+    position: 'left',
+    type: 'text',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    date: new Date(),
+  },
+  {
+      position: 'left',
+      type: 'text',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+      date: new Date(),
+  },
+  {
+    position: 'left',
+    type: 'text',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    date: new Date(),
+},
+{
+  position: 'left',
+  type: 'text',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+  date: new Date(),
+},
+{
+  position: 'right',
+  type: 'text',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+  date: new Date(),
+},
+]
 
 const Chat = (props) => {   
     // const [count, setCount] = useState(0);
     // useEffect(() => {
 // 
     // }, []);
-
     return (
-      <div id="Chat" >
+      <>
+        <p></p>
         <p>Chat</p>
-      </div>
+        <div className="Chat-Container">
+
+          <MessageList
+          className='message-list'
+          lockable={true}
+          toBottomHeight={'100%'}
+          dataSource={messages1}
+          />
+
+        </div>
+        
+      </>
+      
     );
 
 
